2fbb86ce3a5ee3df248f42d62f58795eaaded492
Neelesh Rajpurohit <nrajpurohit@tavisca.com>
2019-08-14 16:21:20 +0530

    Added a test

---
+++ TestAPI/UnitTest1.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+using WebApplication1.Controllers;
+
+namespace TestAPI
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            ValuesController control = new ValuesController();
+            var result = control.Get(2000);
+            Assert.Equal(result, "Leap Year");
+        }
+    }
+}
